458b5d9ad495177ef5f3ea4749390fdebaf852e1
ddarahakit2023 <ddarahakit2023@gmail.com>
2024-03-07 10:38:26 +0900

    [Feat] 코스 개설 기능 테스트 (#4)
    테스트 코드 추가

---
+++ backend/src/test/java/com/sjb/api/learning/course/CourseServiceTest.java
@@ -0,0 +1,15 @@
+package com.sjb.api.learning.course;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CourseServiceTest {
+
+    @Test
+    void createCourse() {
+    }
+}
